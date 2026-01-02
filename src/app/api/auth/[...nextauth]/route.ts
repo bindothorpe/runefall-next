54087df94c2271868c0401e5a9f0fd7b337132b5
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2026-01-02 23:31:28 +0100

    feat: :sparkles: Store session as JWT inside client

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,51 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import type { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/prisma";
 
 //TODO: Use Prisma for database objects
 // Add custom User type that includes id
 declare module "next-auth" {
-    interface Session {
-        user: {
-            id?: string;
-            name?: string | null;
-            email?: string | null;
-            image?: string | null;
-        }
-    }
+  interface Session {
+    user: {
+      id?: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
 }
 
 // Define the auth options with proper typing
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-    ],
-    pages: {
-        signIn: '/signin',
-        error: '/signin',
-        signOut: '/'
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: "/signin",
+    error: "/signin",
+    signOut: "/",
+  },
+  debug: process.env.NODE_ENV === "development",
+  callbacks: {
+    // Handle redirection after signin
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
     },
-    debug: process.env.NODE_ENV === 'development',
-    callbacks: {
-        // Handle redirection after signin
-        async redirect({ url, baseUrl }) {
-            // Allows relative callback URLs
-            if (url.startsWith("/")) return `${baseUrl}${url}`;
-            // Allows callback URLs on the same origin
-            else if (new URL(url).origin === baseUrl) return url;
-            return baseUrl;
-        },
-    },
-    secret: process.env.NEXTAUTH_SECRET
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 };
 // Create the handler using the auth options
 const handler = NextAuth(authOptions);
